76a01f7a1de166257b525a2277d28ea2f4a74b20
louieliet <louieliet2003@gmail.com>
2022-05-03 21:11:57 -0500

    start
    Co-authored-by: lizzytrujillosalgado130503@gmail.com <lizzytrujillosalgado130503@gmail.com>

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from numpy import numpy
 import pygame, sys
 from pygame.locals import*
+
+pygame.init()
+win=pygame.display.set_mode((500,500))
+img=pygame.imagen.load("descargar.png")
+white=(255,255,255)
+x=10
+y=10
+while True:
+    for event in pygame.evente.ger():
+        
