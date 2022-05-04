0b2fcff95ed40cae716798d31ee0112319e628c4
louieliet <louieliet2003@gmail.com>
2022-05-03 21:46:21 -0500

    o

--- main.py
+++ main.py
@@ -1,39 +1,46 @@
+from lib2to3.pygram import python_grammar_no_print_statement
 import numpy as np
 import pygame, sys
 from pygame.locals import*
+
 
 a=np.array([[2,4,6],[4,5,6],[3,1,-2]])
 b=np.array([18,24,4])
 x=np.linalg.solve(a,b)
 print(x)
 
+
 pygame.init()
-win=pygame.display.set_mode((500,500))
-img=pygame.imagen.load("unkown.png")
+pygame.mouse.set_cursor(*pygame.cursors.diamond)
+bg=pygame.image.load("unknown.png")
+win=pygame.display.set_mode((800,800))
+pygame.display.set_caption("Proyecto Álgebra")
+img=pygame.image.load("unknown.png")
 white=(255,255,255)
 x=10
 y=10
 while True:
-    for event in pygame.evente.ger():
+    #gameDisplay.blit(bg,(800,800))
+    for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
         if event.type==pygame.KEYDOWN:
             if event.key==K_w:
-                y=y-20
+                y=y-80
             if event.key==K_s:
-                y=y+20
+                y=y+80
             if event.key==K_d:
-                x=x+20
+                x=x+80
             if event.key==K_a:
-                x=x-20
+                x=x-80
     win.fill(white)
     win.blit(img,(x,y))
     pygame.display.update()
 
 
 
-            
+    
 
 
 
