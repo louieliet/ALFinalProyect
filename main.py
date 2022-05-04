97d4aa2e76d26c7053e0e04c47a6fb0146f46619
louieliet <louieliet2003@gmail.com>
2022-05-03 22:09:53 -0500

    03/05

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from lib2to3.pygram import python_grammar_no_print_statement
 import numpy as np
 import pygame, sys
 from pygame.locals import*
+import math
+import random
+from pygame import mixer
 
 
 a=np.array([[2,4,6],[4,5,6],[3,1,-2]])
@@ -10,17 +13,28 @@ x=np.linalg.solve(a,b)
 print(x)
 
 
+#Player
+quintero=pygame.image.load("quintero.jpg")
+link=pygame.image.load("icon.png")
+linkchiquito=pygame.transform.scale(link,(50,50))
+
+#Init
 pygame.init()
-pygame.mouse.set_cursor(*pygame.cursors.diamond)
-bg=pygame.image.load("unknown.png")
-win=pygame.display.set_mode((800,800))
+win=pygame.display.set_mode((1200,800))
 pygame.display.set_caption("Proyecto Álgebra")
-img=pygame.image.load("unknown.png")
+pygame.display.set_icon(linkchiquito)
+
+#Cursor
+pygame.mouse.set_cursor(*pygame.cursors.diamond)
+
+#White
 white=(255,255,255)
+
+#Atributos y while:
 x=10
 y=10
 while True:
-    #gameDisplay.blit(bg,(800,800))
+    
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -34,8 +48,9 @@ while True:
                 x=x+80
             if event.key==K_a:
                 x=x-80
+
     win.fill(white)
-    win.blit(img,(x,y))
+    win.blit(linkchiquito,(x,y))
     pygame.display.update()
 
 
