60c3ad26692ddd52c3a71e78d1d0e9c558ce35ca
louieliet <louieliet2003@gmail.com>
2022-05-07 21:45:49 -0500

    07/05

--- mainv2.py
+++ mainv2.py
@@ -1,4 +1,4 @@
-import pygame, sys, math, random, os
+import pygame, sys, math, random 
 from pygame.locals import *
 from pygame import mixer
 
@@ -9,6 +9,7 @@ pygame.init()
 pygame.display.set_caption("Proyecto Álgebra Lineal")
 screen = pygame.display.set_mode(size, 0, 32)
 font = pygame.font.SysFont(None, 20)
+menutitle = pygame.font.SysFont(None, 45)
 clock=pygame.time.Clock()
 
 #Colors
@@ -34,12 +35,15 @@ def mainmenu(click):
     while True:
 
         #What it shows in the screen
-        screen.fill(WHITE)
-        escribir("-- Proyecto Álgebra Lineal --",font,BLACK,screen,20,20)
+
+        menubg = pygame.image.load("mathbg.jpg") 
+        menubg = pygame.transform.scale(menubg,size)  
+        screen.blit(menubg,(0,0))
+        escribir("-- Proyecto Álgebra Lineal --",menutitle,WHITE,screen,45,50)
 
         mx,my = pygame.mouse.get_pos()
-        button_1 = pygame.Rect(100,100,200,50)
-        button_2 = pygame.Rect(100,200,200,50)
+        button_1 = pygame.Rect(145,160,200,50)
+        button_2 = pygame.Rect(145,240,200,50)
 
         if button_1.collidepoint((mx,my)):
             if click:
@@ -50,10 +54,10 @@ def mainmenu(click):
                 credits()
         
         pygame.draw.rect(screen,RED,button_1)
-        menu_font=pygame.font.SysFont("Hevetica",35)
-        escribir("Consultar Mapa",menu_font,BLACK,screen,105,110)
+        menu_font=pygame.font.SysFont("Hevetica",30)
+        escribir("Consultar Mapa",menu_font,WHITE,screen,165,175)
         pygame.draw.rect(screen,RED,button_2)
-        escribir("Creditos",menu_font,BLACK,screen,145,215)
+        escribir("Creditos",menu_font,WHITE,screen,200,255)
         click = False
 
         #We define what does every key do
@@ -117,29 +121,29 @@ def up_map():
             
             if event.type == pygame.KEYDOWN:
                 if event.key==K_w:
-                    yspeed = -0.5
+                    yspeed = -1
                     usercopy=pygame.transform.rotate(user,0)
                 if event.key==K_s:
-                    yspeed = 0.5
+                    yspeed = 1
                     usercopy=pygame.transform.rotate(user,180)
                 if event.key==K_d:
-                    xspeed = 0.5
+                    xspeed = 1
                     usercopy=pygame.transform.rotate(user,-90)
                 if event.key==K_a:
-                    xspeed = -0.5
+                    xspeed = -1
                     usercopy=pygame.transform.rotate(user,90)
 
                 if event.key==K_UP:
-                    y2speed = -0.5
+                    y2speed = -1
                     user2copy=pygame.transform.rotate(user2,0)
                 if event.key==K_DOWN:
-                    y2speed = 0.5
+                    y2speed = 1
                     user2copy=pygame.transform.rotate(user2,180)
                 if event.key==K_RIGHT:
-                    x2speed = 0.5
+                    x2speed = 1
                     user2copy=pygame.transform.rotate(user2,-90)
                 if event.key==K_LEFT:
-                    x2speed = -0.5
+                    x2speed = -1
                     user2copy=pygame.transform.rotate(user2,90)
 
                 if event.key == K_ESCAPE:
@@ -174,7 +178,7 @@ def up_map():
         y2 += y2speed
         user2_position = (x2,y2)
    
-        dist = math.sqrt((x-x2)**2 + (y-y2)**2)
+        dist = int(math.sqrt((x-x2)**2 + (y-y2)**2))
 
         kcoo_font = pygame.font.SysFont("Helvetica",15)
         mcoo_font = pygame.font.SysFont("Helvetica",15)
@@ -190,7 +194,7 @@ def up_map():
         screen.blit(usercopy,(x,y))
         screen.blit(user2copy,(x2,y2))
         screen.blit(kcoo,(0,460))
-        screen.blit(mcoo,(0,485))
+        screen.blit(mcoo,(0,480))
  
 
         pygame.display.update()
